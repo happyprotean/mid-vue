d9e3145b9d4097d863d45f91f1e4397534d70995
“fanhuanxi” <fanhuanxi@meituan.com>
2022-04-11 22:39:57 +0800

    scheduler done

--- src/reactity/effect.ts
+++ src/reactity/effect.ts
@@ -1,5 +1,5 @@
 // activeEffect用于保存当前正在生效的effect
-let activeEffect
+let activeEffect;
 
 // targetMap: {
 //   obj1: {
@@ -11,49 +11,51 @@ let activeEffect
 //     key2: dep[],
 //   }
 // }
-// 用于保存所有响应式对象的依赖 
-let targetMap = new Map()
-
+// 用于保存所有响应式对象的依赖
+let targetMap = new Map();
 
 class ReactiveEffect {
-  private _fn: any
-  constructor(fn){
-    this._fn = fn
+  private _fn: any;
+  constructor(fn, public scheduler?) {
+    this._fn = fn;
   }
-  run(){
+  run() {
     // 将当前执行的effect保存到全局变量上，便于添加依赖
-    activeEffect = this
-    return this._fn()
+    activeEffect = this;
+    return this._fn();
   }
 }
 
-
-export function effect(fn) {
-  let _effect = new ReactiveEffect(fn)
-  _effect.run()
-  return _effect.run.bind(_effect)
+export function effect(fn, options: any = {}) {
+  let _effect = new ReactiveEffect(fn, options.scheduler);
+  _effect.run();
+  return _effect.run.bind(_effect);
 }
 
 export function track(target, key) {
-  let depsMap = targetMap.get(target)
-  if (!depsMap){
-    depsMap = new Map()
-    targetMap.set(target, depsMap)
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
   }
 
-  let deps = depsMap.get(key)
-  if (!deps){
-    deps = new Set()
-    depsMap.set(key, deps)
+  let deps = depsMap.get(key);
+  if (!deps) {
+    deps = new Set();
+    depsMap.set(key, deps);
   }
 
-  deps.add(activeEffect)
+  deps.add(activeEffect);
 }
 
 export function trigger(target, key) {
-  let depsMap = targetMap.get(target)
-  let deps = depsMap.get(key)
+  let depsMap = targetMap.get(target);
+  let deps = depsMap.get(key);
   for (const dep of deps) {
-    dep.run() 
+    if (dep.scheduler) {
+      dep.scheduler();
+    } else {
+      dep.run();
+    }
   }
 }
--- src/reactity/tests/effect.spec.ts
+++ src/reactity/tests/effect.spec.ts
@@ -10,7 +10,11 @@ describe("effect", () => {
     effect(() => {
       nextAge = user.age + 1;
     });
+
+    // effect中会自动调用其fn
     expect(nextAge).toBe(11);
+
+    // 当依赖触发时，会再次执行effect中的fn
     user.age++;
     expect(nextAge).toBe(12);
   });
@@ -20,9 +24,39 @@ describe("effect", () => {
       foo++
       return 'foo'
     })
+
+    // effect中会自动调用其fn
     expect(foo).toBe(2)
+
+    // 通过runner再次执行effect中的fn，并获取其返回值
     let r = runner()
     expect(foo).toBe(3)
     expect(r).toBe('foo')
+  });
+  test("scheduler", () => {
+    let dummy
+    let run;
+    const scheduler = jest.fn(() => {
+      run = runner
+    })
+    const obj = reactive({ foo: 1 })
+    const runner = effect(() => {
+      dummy = obj.foo
+    }, {
+      scheduler,
+    })
+
+    // scheduler函数不会在effect初始化时调用
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+
+    //  当响应式对象发生更改时，优先调用scheduler
+    obj.foo++
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    expect(dummy).toBe(1)
+
+    // 通过runner，手动调用effect中的fn
+    run()
+    expect(dummy).toBe(2)
   })
 });
