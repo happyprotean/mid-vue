82b98da3bae89fff6bf8d556f8958deb128e8930
“fanhuanxi” <fanhuanxi@meituan.com>
2022-05-04 20:56:44 +0800

    feat: 实现ref

---
+++ src/reactity/ref.ts
@@ -0,0 +1,39 @@
+import { hasChanged, isObject } from "../util";
+import { trackEffects, triggerEffects, isTracking } from "./effect";
+import { reactive } from "./reactive";
+class RefImpl {
+  private _value: any;
+  private _rawValue: any;
+  public dep;
+  constructor(value) {
+    this._rawValue = value;
+    this._value = convert(value);
+    this.dep = new Set();
+  }
+
+  get value() {
+    trackRefValue(this);
+    return this._value;
+  }
+
+  set value(newVal) {
+    if (!hasChanged(this._rawValue, newVal)) return;
+    this._rawValue = newVal;
+    this._value = convert(newVal);
+    triggerEffects(this.dep);
+  }
+}
+
+function trackRefValue(ref) {
+  if (isTracking()) {
+    trackEffects(ref.dep);
+  }
+}
+
+function convert(value) {
+  return isObject(value) ? reactive(value) : value;
+}
+
+export function ref(value) {
+  return new RefImpl(value);
+}
