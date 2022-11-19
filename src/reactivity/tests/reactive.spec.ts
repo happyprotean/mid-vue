e52db6a34b65f6df5063681f9b07badffe875b82
“fanhuanxi” <fanhuanxi@meituan.com>
2022-11-19 20:24:21 +0800

    feat: 实现isReadonly, isReactive

--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -1,9 +1,11 @@
-import { reactive } from '../reactive'
+import { reactive, isReactive } from '../reactive'
 describe("reactive", () => {
   test("happy path", () => {
     const original = { foo: 11 }
     const observed = reactive(original) 
     expect(observed).not.toBe(original)
     expect(observed.foo).toBe(11)
+    expect(isReactive(observed)).toBe(true)
+    expect(isReactive(original)).toBe(false)
   })
 })
