6f2e46916d6ccbae1f0d89bf72e50fd6afa77214
“fanhuanxi” <fanhuanxi@meituan.com>
2022-11-28 23:53:27 +0800

    feat: 2-1 初始化component主流程

---
+++ src/runtime-core/component.ts
@@ -0,0 +1,40 @@
+
+export function createComponentInstance(vnode) {
+  const component = {
+    vnode,
+    type: vnode.type,
+  }
+  return component
+}
+
+export function setupComponent(instance) {
+  // TODO:
+  // initProps
+  // initSlots 
+  setupStatefulComponent(instance)
+}
+
+function setupStatefulComponent(instance) {
+  const component = instance.type
+  const { setup } = component
+  if (setup) {
+    const setupResult = setup()
+    handleSetupResult(instance, setupResult)
+  }
+}
+
+function handleSetupResult(instance, setupResult) {
+  // TODO: function
+  if (typeof setupResult === 'object') {
+    instance.setupState = setupResult
+  }
+  finishComponentSetup(instance)
+}
+
+function finishComponentSetup(instance) {
+  const component = instance.type
+  if (component.render) {
+    instance.render = component.render
+  }
+}
+
